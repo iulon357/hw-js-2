8f67aad66ec31a7971066627523eaa61e798ca66
iulon <i.magularia357@gmail.com>
2023-07-18 00:17:52 +0400

    v4

--- app.js
+++ app.js
@@ -9,3 +9,30 @@ console.log(numberArrays);
 
 //დავალება-2:
 
+const arr1 = ["Samsung", 1899, "very good"]
+
+//დავალება-3:
+
+const arr2 = [98, 78, 14, 71, 65];
+console.log(arr2);
+const num1 = arr2[0];
+const num2 = arr2[1];
+const num3 = arr2[2];
+const num4 = arr2[3];
+const num5 = arr2[4];
+const arrLength = arr2.length;
+console.log("საშუალო რიცხვია " +(num1 + num2 + num3 + num4 + num5)/ arrLength);
+
+//დავალება-4:
+
+const mobShop = {
+    phoneName: "Samsung S23",
+    phoneDescription: "New Stolen",
+    phonePrice: 1899,
+    shopAddresses: "K. Gamsakhurdia (Beijing) Avenue N14a,Tbilisi,Georgia",
+    phoneMemoryInGB: 128,
+}
+
+//დავალება-5:
+
+console.log(" Phone name is " + mobShop.phoneName + ", phone price is " + mobShop.phonePrice + " and you can buy it at " + mobShop.shopAddresses)
