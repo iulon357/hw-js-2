f918496ac5c472a4d99117fa6614ff2c7922ebc6
iulon <i.magularia357@gmail.com>
2023-07-17 23:18:02 +0400

    v3

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 //დავალება-1:
 
+const numberArrays = [2, 4, 6, 8, 10, 12];
+console.log(numberArrays);
+numberArrays.pop();
+console.log(numberArrays);
+numberArrays.unshift(12);
+console.log(numberArrays);
+
+//დავალება-2:
+
